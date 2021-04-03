a95515ca78c3c34080c50ed6f488a45fce787c24
Alexey Barinov <begoodbebad2012@gmail.com>
2021-04-03 19:34:16 +0300

    Added

--- StepperApp/ViewController.swift
+++ StepperApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloLabel: UILabel!
+    @IBOutlet var stepper: UIStepper!
+    
+    let fontName = "AmericanTypewriter"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stepper.minimumValue = 8
+        stepper.maximumValue = 40
+        stepper.layer.cornerRadius = 5
+        
+        stepper.value = Double(helloLabel.font.pointSize)
     }
 
-
+    @IBAction func stepperAction() {
+        helloLabel.font = UIFont(name: fontName, size: CGFloat(stepper.value))
+    }
+    
 }
 
